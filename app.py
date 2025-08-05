218b297a174131f66c5a82ca86b97c8cf351a1bb
Pranav_Ozarde <167470161+PranavOzarde32@users.noreply.github.com>
2025-08-06 02:15:38 +0530

    Add files via upload
    Jupyter Notebook And main file is app.py

---
+++ app.py
@@ -0,0 +1,76 @@
+import streamlit as st
+import pickle
+import requests
+
+
+def fetch_poster(movie_id):
+    try:
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=cbd8ad0c3cec6abf735656cde4dcc469&language=en-US"
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        return "https://image.tmdb.org/t/p/w500/" + data["poster_path"]
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+        return "https://via.placeholder.com/150?text=No+Image"
+    except KeyError:
+        print("Poster path not found in API response")
+        return "https://via.placeholder.com/150?text=No+Image"
+    except ValueError as ve:
+        print("Invalid JSON returned from API")
+        return "https://via.placeholder.com/150?text=No+Image"
+
+
+
+
+# Load pickled files
+movies_df = pickle.load(open('movies.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+
+def recommend(movie_title):
+    movie_index = movies_df[movies_df['title'] == movie_title].index[0]
+    distances = similarity[movie_index]
+    recommended_movie_posters = []
+    # Get top 5 similar movies (excluding itself)
+    similar_movies = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    for i in similar_movies:
+        movie_id = movies_df.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movies.append(movies_df.iloc[i[0]].title)
+    return recommended_movies,recommended_movie_posters
+
+
+# Streamlit UI
+st.title('Movie Recommender System')
+
+movie_titles = movies_df['title'].values
+selected_movie_name = st.selectbox("Select a movie to get recommendations:", movie_titles)
+
+if st.button("Recommend"):
+    names, posters = recommend(selected_movie_name)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
+
